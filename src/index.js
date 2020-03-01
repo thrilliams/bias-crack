505b879337898e101f763412fdfdc2b38778ce82
Soren <soren@aethre.co>
2020-02-29 22:27:36 -0800

    Initial commit!
    Co-authored-by: SmashMineGame <aryanp@khanlabschool.org>
    Co-authored-by: wachuuski <wachuuski@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,30 +1,107 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import logo from './logo.svg';
+import './main.scss';
+import { newsapi, particles } from './config.json';
+import { articles } from './articleCache.json';
+import Particles from 'react-particles-js';
+import * as imgerr from './404.png';
+import navbar from './navbar.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function fetchNews(method, params) {
+    let searchParams = new URLSearchParams();
+    for (let key in params) {
+        searchParams.append(key, params[key]);
+    }
+
+    let response = await fetch(`http://newsapi.org/v2/${method}?apiKey=${newsapi}${searchParams.toString() ? '&' + searchParams.toString(): ''}`);
+    return await response.json();
 }
 
-export default App;
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = { page: 'home' };
+    }
+
+    setPage(page) {
+        this.setState({ ...this.state, page: page });
+    }
+
+    render() {
+        return (<>
+            <nav>
+                <img src={navbar} alt=""></img>
+            </nav>
+            {this.state.page === 'home' ? <Home /> : <ArticleContainer />}
+            <Particles params={particles} />
+        </>);
+    }    
+}
+
+function Home() {
+    return (<div className="jumbotron">
+        <div className="card">
+            <div className="card-icon"></div>
+            <div className="card-title"></div>
+            <button className="card-btn"></button>
+        </div>
+        <div className="card">
+            <div className="card-icon"></div>
+            <div className="card-title"></div>
+            <button className="card-btn"></button>
+        </div>
+        <div className="card">
+            <div className="card-icon"></div>
+            <div className="card-title"></div>
+            <button className="card-btn"></button>
+        </div>
+    </div>);
+}
+
+class ArticleContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { articles: articles };
+
+        fetchNews('top-headlines', { language: 'en', country: 'us', category: 'general' })
+            .then(JSON.stringify)
+            .then(console.log);
+
+        // fetchNews('top-headlines', { language: 'en', country: 'us' })
+        //     .then(response => this.setState({ ...this.state, articles: response.articles }));
+    }
+
+    render() {
+        if (this.state.articles.length > 0) {
+            return (<div id="article-container">
+                {this.state.articles.map((e, i) => <Article key={i} article={e} />)}
+            </div>);
+        } else {
+            return <p>Loading...</p>
+        }
+    }
+}
+
+function Article(props) {
+    let a = { ...props.article };
+    a.title = a.title.split(' - ');
+    let source = a.title[1];
+    a.title = a.title[0].split(' | ')[0];
+    if (a.content && a.content.match(/\[\+[0-9]{4,} chars\]/g)) {
+        a.content = a.content.slice(0, a.content.indexOf(a.content.match(/\[\+[0-9]{4,} chars\]/g)));
+    } else {
+        a.content = undefined;
+    } 
+    return (<article className="article" key={props.i}>
+        <img onClick={_ => window.open(a.url)} src={a.urlToImage} alt={a.source && a.source.name} onError={e => e.target.src = imgerr}></img>
+        <p className="content">{a.content}</p>
+        <div className="article-title">
+          <h2 className="title" onClick={_ => window.open(a.url)}>{a.title}</h2>
+          <h4 className="source">{source}</h4>
+        </div>
+        <h3 className="description">{a.description}</h3>
+    </article>);
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
