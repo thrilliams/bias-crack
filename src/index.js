d2001b72fa7070bdacb7526c6610c295e6d3d70e
Soren <soren@aethre.co>
2020-03-01 10:27:10 -0800

    final commit!
    Co-authored-by: SmashMineGame <aryanp@khanlabschool.org>

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.scss';
-import { newsapi, particles } from './config.json';
+import { /*newsapi, */particles } from './config.json';
 import { articles } from './articleCache.json';
 import Particles from 'react-particles-js';
 import * as imgerr from './img/404.png';
-import navbar from './img/brand/navbar.svg';
+import navbar from './img/navbar.svg';
 
-import eye from './img/icons/eye.svg';
-import scale from './img/icons/scale.svg';
-import search from './img/icons/search.svg';
-import handshake from './img/icons/handshake.svg';
+import homedata from './home.json';
+const possibleTags = [['authority', 'liberty'], ['nation', 'world'], ['progress', 'tradition'], ['equality', 'market']];
 
-async function fetchNews(method, params) {
-    let searchParams = new URLSearchParams();
-    for (let key in params) {
-        searchParams.append(key, params[key]);
-    }
+function randomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
 
-    let response = await fetch(`http://newsapi.org/v2/${method}?apiKey=${newsapi}${searchParams.toString() ? '&' + searchParams.toString(): ''}`);
-    return await response.json();
+function randomTags() {
+    return possibleTags.map(e => randomElement(e));
 }
 
 class App extends React.Component {
@@ -49,34 +45,51 @@ class App extends React.Component {
                 <p className="clickable" onClick={this.setPageHome.bind(this)}>Home</p>
                 <p className="clickable" onClick={this.setPageFeed.bind(this)}>Feed</p>
             </nav>
-            {this.state.page === 'home' ? <Home /> : <ArticleContainer />}
+            {{
+                home: <Home />,
+                feed: <ArticleContainer />
+            }[this.state.page]}
             <Particles params={particles} />
         </>);
     }
 }
 
 function Home() {
-    return (<div id="jumbotron">
-        <div className="card">
-            <img className="card-icon" src={scale} alt="" />
-            <div className="card-title">Encouraging Open-Mindedness</div>
-            <button className="card-btn">Learn More</button>
+    return (<div className="content">
+        <div className="blurb">
+            <h3 className="blurb-title">Bias Crack</h3>
+            <p>One of the biggest challenges faced by the average knowledge-seeker is that the media of today has the glaring issue of a self propagating cycle of extremist exponentiation. That is, since news sites use algorithms to determine your views and feed you standpoints that you already agree with, it encourages readers to be pushed towards the extreme ends of whatever political leaning they may be (however slightly) inclined towards. Political extremism tends to have the issue of forming echo chambers, circles of confirmation that are exceptionally resistant to outside ideas that don’t fit with their orthodox.</p>
+            <p>The solution? Expose cognizant readers to dissent to allow for a more open mind and the ability to see the validity in arguments of those that they don’t necessarily agree with. Bias Crack takes note of your political and ideological leanings and intentionally showing the viewer a mix of views they agree with as well as those that provide dissent for a healthy balanced diet of ideas.</p>
         </div>
-        <div className="card">
-            <img className="card-icon" src={handshake} alt="" />
-            <div className="card-title">Developing Understanding</div>
-            <button className="card-btn">Learn More</button>
+        <div id="jumbotron">
+            {[...homedata].map((e, i) => <Card key={i} entry={e} />)}
         </div>
-        <div className="card">
-            <img className="card-icon" src={search} alt="" />
-            <div className="card-title">Improving Clarity</div>
-            <button className="card-btn">Learn More</button>
-        </div>
-        <div className="card">
-            <img className="card-icon" src={eye} alt="" />
-            <div className="card-title">Broadening Perspectives</div>
-            <button className="card-btn">Learn More</button>
-        </div>
+        {[...homedata].map((e, i) => <Description key={i} entry={e} />)}
+    </div>);
+}
+
+function Card(props) {
+    let name = [props.entry.name.slice(0, props.entry.name.indexOf(' ')), props.entry.name.slice(props.entry.name.indexOf(' ') + 1)]
+    console.log(name)
+    return (<div className="card">
+        <img className="card-icon" src={`${process.env.PUBLIC_URL}/icons/${props.entry.icon}.svg`} alt={props.entry.icon} title={props.entry.icon} />
+        <div className="card-title">{name[0]}<br />{name[1]}</div>
+        <button className="card-btn clickable" onClick={_ => document.querySelector(`#${props.entry.id}`).scrollIntoView({ behavior: 'smooth' })}>Learn More</button>
+    </div>);
+}
+
+function Description(props) {
+    let body = props.entry.body.split('\n');
+    if (body.length > 1) {
+        body = body.reduce((previous, current) => <>{previous}<p>{current}</p></>, <></>);
+    } else {
+        body = <p>{body[0]}</p>;
+    }
+
+    return (<div className="blurb" id={props.entry.id}>
+        <img className="blurb-icon" src={`${process.env.PUBLIC_URL}/icons/${props.entry.icon}.svg`} alt={props.entry.icon} title={props.entry.icon} />
+        <h3 className="blurb-title">{props.entry.name}</h3>
+        {body}
     </div>);
 }
 
@@ -84,18 +97,11 @@ class ArticleContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = { articles: articles };
-
-        fetchNews('top-headlines', { language: 'en', country: 'us', category: 'general' })
-            .then(JSON.stringify)
-            .then(console.log);
-
-        // fetchNews('top-headlines', { language: 'en', country: 'us' })
-        //     .then(response => this.setState({ ...this.state, articles: response.articles }));
     }
 
     render() {
         if (this.state.articles.length > 0) {
-            return (<div id="article-container">
+            return (<div id="article-container" className="content">
                 {this.state.articles.map((e, i) => <Article key={i} article={e} />)}
             </div>);
         } else {
@@ -113,15 +119,16 @@ function Article(props) {
         a.content = a.content.slice(0, a.content.indexOf(a.content.match(/\[\+[0-9]{4,} chars\]/g)));
     } else {
         a.content = undefined;
-    } 
+    }
     return (<article className="article" key={props.i}>
         <img className="clickable" onClick={_ => window.open(a.url)} src={a.urlToImage} alt={a.source && a.source.name} onError={e => e.target.src = imgerr}></img>
         <p className="content">{a.content}</p>
         <div className="article-title">
-          <h2 className="clickable title" onClick={_ => window.open(a.url)}>{a.title}</h2>
-          <h4 className="source">{source}</h4>
+            <h2 className="clickable title" onClick={_ => window.open(a.url)}>{a.title}</h2>
+            <h4 className="source">{source}</h4>
         </div>
         <h3 className="description">{a.description}</h3>
+        <ul className="tags">{randomTags().map(tag => <li className="tag"><img src={`${process.env.PUBLIC_URL}/icons/8values/${tag}.svg`} alt={tag} title={tag}></img></li>)}</ul>
     </article>);
 }
 
